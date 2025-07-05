82ce5bce26d3e46f035b735c92aee3a8ccc97e74
Byswork <byswork@yandex.ru>
2025-07-06 00:10:29 +0300

    Сделаны доработки в валидации параметров

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,8 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.annotation.ReleaseDate;
 
 import java.time.LocalDate;
 
@@ -11,8 +15,12 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class Film {
     private Long id;
+    @NotBlank(message = "Название фильма не может быть пустым")
     private String name;
+    @Size(max = (200), message = "Длина описания превышает 200 символов")
     private String description;
+    @ReleaseDate(value = "1895-12-28", message = "Дата релиза не может быть раньше 28 декабря 1865 года")
     private LocalDate releaseDate;
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
     private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,8 +13,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class User {
     private Long id;
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Неверный формат электронной почты")
     private String email;
+    @NotNull(message = "Логин не должен быть пустым")
+    @NotBlank(message = "Логин не должен содержать пробелы")
     private String login;
     private String name;
+    @Past(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
 }
