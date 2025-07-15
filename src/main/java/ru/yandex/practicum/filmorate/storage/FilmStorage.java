c6a1d34bbfec6bfb90ae93279f8df90887ec88ed
Byswork <byswork@yandex.ru>
2025-07-15 23:36:46 +0300

    Исправлены ошибки в codestyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -5,8 +5,12 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film update(Film newFilm);
+
     Collection<Film> findAll();
+
     long getNextId();
+
     Film getFilmById(Long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -5,8 +5,12 @@ import java.util.Collection;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User newUser);
+
     Collection<User> findAll();
+
     long getNextId();
+
     User getUserById(Long id);
 }
