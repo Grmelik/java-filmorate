bbdcc3baeca64f9560e1c43921dad1a68f7b0cc4
Byswork <31487750+Grmelik@users.noreply.github.com>
2025-07-25 00:32:04 +0300

    Add files via upload

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,30 @@
+insert into GenreNames(GenreID, GenreName) values(1, 'Комедия')
+/
+insert into GenreNames(GenreID, GenreName) values(2, 'Драма')
+/
+insert into GenreNames(GenreID, GenreName) values(3, 'Мультфильм')
+/
+insert into GenreNames(GenreID, GenreName) values(4, 'Триллер')
+/
+insert into GenreNames(GenreID, GenreName) values(5, 'Документальный')
+/
+insert into GenreNames(GenreID, GenreName) values(6, 'Боевик')
+/
+insert into GenreNames(GenreID, GenreName) values(7, 'Фантастика')
+/
+insert into GenreNames(GenreID, GenreName) values(8, 'Фэнтези')
+/
+insert into GenreNames(GenreID, GenreName) values(9, 'Приключения')
+/
+insert into Ratings(RatingID, RatingName) values(1, 'G')
+/
+insert into Ratings(RatingID, RatingName) values(2, 'PG')
+/
+insert into Ratings(RatingID, RatingName) values(3, 'PG')
+/
+insert into Ratings(RatingID, RatingName) values(4, 'R')
+/
+insert into Ratings(RatingID, RatingName) values(5, 'NC-17')
+/
+commit
+/
